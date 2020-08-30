66f34703182b27d6465e2d8bd6b21750e213b633
Christian Otu <christianotu.d@gmail.com>
2020-08-30 20:09:59 +0100

    Updates

---
+++ precache-manifest.32aab5a890930877616cb119c241cd64.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569db51642d30a41644901ce8b7c65eb",
+    "url": "/FEM-Easybank/index.html"
+  },
+  {
+    "revision": "41118b6720d475ca542c",
+    "url": "/FEM-Easybank/static/css/main.f4f88b3f.chunk.css"
+  },
+  {
+    "revision": "d6b5ee495a25651cf82e",
+    "url": "/FEM-Easybank/static/js/2.145b088d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FEM-Easybank/static/js/2.145b088d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41118b6720d475ca542c",
+    "url": "/FEM-Easybank/static/js/main.c1137643.chunk.js"
+  },
+  {
+    "revision": "4813adeeab2616baa7be",
+    "url": "/FEM-Easybank/static/js/runtime-main.6f161db1.js"
+  },
+  {
+    "revision": "c23dd1d0b7e4b4a1637d50cbb9fff810",
+    "url": "/FEM-Easybank/static/media/bg-intro-mobile.c23dd1d0.svg"
+  },
+  {
+    "revision": "db3d89069f8292defec0f180493a8817",
+    "url": "/FEM-Easybank/static/media/icon-api.db3d8906.svg"
+  },
+  {
+    "revision": "d4cdc9d6b3c930ddaff8dc79acc005a1",
+    "url": "/FEM-Easybank/static/media/icon-budgeting.d4cdc9d6.svg"
+  },
+  {
+    "revision": "1a81861b7aaa8c30169d65faf9027218",
+    "url": "/FEM-Easybank/static/media/icon-facebook.1a81861b.svg"
+  },
+  {
+    "revision": "e0814599d1e638a04578e79a652801c0",
+    "url": "/FEM-Easybank/static/media/icon-instagram.e0814599.svg"
+  },
+  {
+    "revision": "2fc9c776f2bb1b98e1d23bc813261bd3",
+    "url": "/FEM-Easybank/static/media/icon-onboarding.2fc9c776.svg"
+  },
+  {
+    "revision": "7a9111dbfa3b4b2ea0d088435bfe1d76",
+    "url": "/FEM-Easybank/static/media/icon-online.7a9111db.svg"
+  },
+  {
+    "revision": "ef15af23a9c6edf1014f6f44b31c8b78",
+    "url": "/FEM-Easybank/static/media/icon-pinterest.ef15af23.svg"
+  },
+  {
+    "revision": "064c31714c77b6580c3e4e8176c435fb",
+    "url": "/FEM-Easybank/static/media/icon-twitter.064c3171.svg"
+  },
+  {
+    "revision": "8e2f566fcefd23c8aacdaa5a83cdaff2",
+    "url": "/FEM-Easybank/static/media/icon-youtube.8e2f566f.svg"
+  },
+  {
+    "revision": "1510e4a211f7a044236decc6d9b4afe9",
+    "url": "/FEM-Easybank/static/media/image-confetti.1510e4a2.jpg"
+  },
+  {
+    "revision": "0f583e2b521e1590dd7ab8366c478227",
+    "url": "/FEM-Easybank/static/media/image-currency.0f583e2b.jpg"
+  },
+  {
+    "revision": "edc63e85927303b6c172617a5d1742b6",
+    "url": "/FEM-Easybank/static/media/image-mockups.edc63e85.png"
+  },
+  {
+    "revision": "c619f6cb0554ff2c465d2bb6268cb493",
+    "url": "/FEM-Easybank/static/media/image-plane.c619f6cb.jpg"
+  },
+  {
+    "revision": "b561f8d50067b0ce1c2d4e8b54671922",
+    "url": "/FEM-Easybank/static/media/image-restaurant.b561f8d5.jpg"
+  },
+  {
+    "revision": "fe5c6dcee699e86a25a18112801ec30f",
+    "url": "/FEM-Easybank/static/media/logo.fe5c6dce.svg"
+  }
+]);
